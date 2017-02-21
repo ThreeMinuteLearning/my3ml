b22ca804bae5aa5adc538a249ad220bff41c364d
Luke Taylor <tekul.hs@gmail.com>
2017-02-21 21:26:30 +0000

    Better story HTML content parsing and JSON output
    Cleans up the content part of each story considerably, parsing out keywords
    removing unnecessary formatting, adding emphasis/bold markup, parsing links
    and emitting JSON which can be fed directly to the front end.

--- parseStories.hs
+++ parseStories.hs
@@ -1,34 +1,103 @@
+{-# LANGUAGE DeriveGeneric #-}
+import GHC.Generics
+import Prelude hiding (id)
 import Text.HTML.TagSoup
 import qualified Data.Text as T
+import qualified Data.ByteString.Lazy as B
+import Data.List (nub)
+import qualified Data.Aeson as J
+
 
 data Story = Story
-    { id_ :: String
+    { id :: String
     , img :: String
     , title :: String
     , tags :: [String]
-    , level :: String
-    , completed :: String
+    , level :: Int
+    , completed :: Int
+    , defineWords :: [String]
     , date :: String
     , content :: String
-    } deriving (Show)
+    } deriving (Show, Generic)
 
+
+instance J.ToJSON Story where
+    toJSON     = J.genericToJSON J.defaultOptions
+    toEncoding = J.genericToEncoding J.defaultOptions
 
 parseSection :: [Tag String] -> Story
 parseSection s =
-    let cols = partitions (~== "<td>") s
+    let
+        cols = partitions (~== "<td>") s
         [image, title, tag1, tag2, tag3, level, completed, date, _] = reverse $ foldl accTxt [] (take 9 cols)
         accTxt ss t = (trim (innerText t)) : ss
         image' = parseImage (head cols)
         ts = filter (not . null) [tag1, tag2, tag3]
         rem = drop 9 cols
         id_ = parseIdFromLink (head rem)
-        content = parseContent (head (tail rem))
+        (content, defs) = parseContent (head (tail rem))
     in
-        Story id_ image' title ts level completed (parseDate date) content
+        Story id_ image' title ts (read level) (read completed) (nub defs) (parseDate date) content
 
 
-parseContent :: [Tag String] -> String
-parseContent = trim . renderTags . takeWhile (~/= "</td>") . drop 1
+parseContent :: [Tag String] -> (String, [String])
+parseContent ts = go ("", []) (drop 1 ts)
+  where
+    go acc [] = acc
+    go acc@(content, defs) (t:ts) =
+        case t of
+            TagOpen "span" [("style", _)] -> go acc ts
+            TagOpen "font" _ -> go acc ts
+            TagClose "font" -> go acc ts
+            TagOpen "div" _ -> go acc ts
+            TagClose "div" -> go acc ts
+            TagOpen "center" _ -> go acc ts
+            TagClose "center" -> go acc ts
+            TagOpen "o:p" _ -> go acc ts
+            TagClose "o:p" -> go acc ts
+            TagClose "span" -> go acc ts
+            TagOpen "span" (("class", "apple-converted-space"):_) -> go acc ts
+            TagOpen "span" (("class", d):_cs) ->
+                if d `elem` ["define","define2","define3","define4"]
+                  then
+                      let
+                         definition = case innerText $ takeWhile (~/= TagClose "span") ts of
+                             "Words in pop-ups" -> ""
+                             txt -> txt
+                      in
+                         go (content ++ definition, definition : defs) (dropWhile (~/= TagClose "span") ts)
+                  else error ("Unhandled span class " ++ d)
+                      -- go acc ts
+            TagOpen "span" [] -> go acc ts
+            TagOpen "em" _ -> go (content ++ "_", defs) ts
+            TagOpen "p" _ -> go (content ++ "\n\n", defs) ts
+            TagOpen "a" _ ->
+                let
+                    url = fromAttrib "href" t
+                    inside = takeWhile (~/= TagClose "a") ts
+                    lnk = "[" ++ innerText inside ++ "](" ++ url ++ ")"
+                in
+                    go (content ++ lnk, defs) (tail $ dropWhile (~/= TagClose "a") ts)
+            TagClose"em" -> go (content ++ "_ ", defs) ts
+            TagOpen "strong\"" _ -> go acc (drop 2 ts) -- specific tag error in J Cooper story
+            TagOpen "strong" _ -> case ts of
+                (TagText s : TagClose "strong" : ts') -> case trim s of
+                    "Words in pop-ups" -> go acc ts'
+                    _ -> go (content ++ "__" ++ s ++ "__", defs) ts'
+                _ -> case head ts of
+                    TagOpen "span" _ -> go acc (tail ts) -- style span within strong
+                    _ -> error ("open strong tag with weird successors" ++ show (head ts))
+            TagOpen "ul" _ -> go acc (dropWhile (~/= "<li>") ts)
+            TagOpen "li" _ ->
+                let
+                  txt = innerText $ takeWhile (~/= TagClose "li") ts
+                in
+                  go (content ++ "\n+" ++ txt, defs) (dropWhile (~/= TagClose "li") ts)
+
+            TagClose "li" -> go acc ts
+            TagClose "ul" -> go acc ts
+            TagClose "td" -> (trim content, defs)
+            _ -> go (content ++ renderTags [t], defs) ts
 
 
 parseImage :: [Tag String] -> String
@@ -57,4 +126,7 @@ main = do
     let secs = parseSections htmlStories
 
         stories = map parseSection secs
-    print stories
+        printStory s = B.putStr s >> putStrLn ","
+    putStrLn "{ \"stories\": ["
+    mapM_ (printStory . J.encode) stories
+    putStrLn "\n]\n}"
