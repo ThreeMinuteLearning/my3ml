73c412db2137ef2043cd2580c4233ebfb93d4b70
Luke Taylor <tekul.hs@gmail.com>
2017-05-23 01:03:37 +0100

    Add a simple spinner from spinkit
    http://tobiasahlin.com/spinkit/
    
    I think the spinner in the elm-spa-example is also taken from this site, but
    the CSS is inlined in the page, so the spinner wasn't appearing.

--- frontend/src/Views/Spinner.elm
+++ frontend/src/Views/Spinner.elm
@@ -1,14 +1,9 @@
 module Views.Spinner exposing (spinner)
 
-import Html exposing (Html, Attribute, div, li)
-import Html.Attributes exposing (class, style)
-import Util exposing ((=>))
+import Html exposing (Html, div, li)
+import Html.Attributes exposing (class)
 
 
 spinner : Html msg
 spinner =
-    li [ class "sk-three-bounce", style ([ "float" => "left", "margin" => "8px" ]) ]
-        [ div [ class "sk-child sk-bounce1" ] []
-        , div [ class "sk-child sk-bounce2" ] []
-        , div [ class "sk-child sk-bounce3" ] []
-        ]
+    li [ class "spinner" ] []
