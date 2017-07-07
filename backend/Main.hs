6facc3072f9531fa2f4bc1e6809ad12dbdc1cc75
Luke Taylor <tekul.hs@gmail.com>
2017-07-07 13:05:58 +0100

    Store the token key in the cwd
    Closes #34.

--- backend/Main.hs
+++ backend/Main.hs
@@ -9,12 +9,17 @@ module Main where
 import           Control.Monad.Logger
 import           Control.Monad.Reader
 import           Crypto.Random (getRandomBytes)
+import qualified Data.Aeson as A
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
 import           Data.Maybe (fromMaybe)
+import           Jose.Jwa
 import           Jose.Jwk
 import           Network.Wai.Handler.Warp (run)
 import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import           Servant
 import           System.Environment (getEnvironment)
+import           System.Directory (doesFileExist)
 
 import           Api.Server (HandlerT, Config(..))
 import qualified Api.Server as Api
@@ -40,15 +45,31 @@ server config assets = enter transform Api.server :<|> serveDirectory assets
     transform :: HandlerT db :~> Handler
     transform = Nat transform'
 
+defaultKeyFile :: FilePath
+defaultKeyFile = "token_key.json"
+
+getKey :: FilePath -> IO Jwk
+getKey file = do
+    exists <- doesFileExist file
+    jwks   <- if exists
+                  then A.decodeStrict <$> B.readFile file
+                  else return Nothing
+    case jwks of
+        Just (JwkSet (k:_)) -> return k
+        _ -> do
+            k  <- getRandomBytes 16
+            let jwk = SymmetricJwk k Nothing Nothing (Just (Encrypted A128KW))
+            BL.writeFile file (A.encode (JwkSet [jwk]))
+            return jwk
+
 
 main :: IO ()
 main = do
     env <- getEnvironment
-    keyBytes <- getRandomBytes 16
+    jwk <- getKey defaultKeyFile
     let port = maybe 8000 read $ lookup "PORT" env
         pgdb = fromMaybe "postgresql://threeml:threeml@localhost/my3ml" $ lookup "PGDB" env
         assets = fromMaybe "assets" $ lookup "ASSETS" env
-        jwk = SymmetricJwk keyBytes Nothing Nothing Nothing
     putStrLn $ "Serving on port " ++ show port ++ "..."
     db <- mkDB pgdb
     stories <- getStories db
