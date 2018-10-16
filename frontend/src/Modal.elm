684da72cad59d39a655d40009b65f75a024b69cc
Luke Taylor <tekul.hs@gmail.com>
2018-10-16 20:08:41 +0100

    Add Modal module which I forgot in a previous commit
    This is needed for for the modal dialog simplification code which was
    committed a while back.

---
+++ frontend/src/Modal.elm
@@ -0,0 +1,30 @@
+module Modal exposing (view)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+
+
+modalHeader : String -> msg -> Html msg
+modalHeader title onClose =
+    div [ class "modal-header"]
+        [ button [ type_ "button", class "close", onClick onClose ]
+            [ span [] [ text "×" ]
+            ]
+        , h3 [ class "modal-title" ] [ text title]
+        ]
+
+
+view : String -> msg -> Html msg -> Html msg
+view title onClose content =
+    div []
+        [ div [ class "modal in", style "display" "block" ]
+            [ div [ class "modal-dialog"]
+                [ div [ class "modal-content"]
+                    [ modalHeader title onClose
+                    , div [ class "modal-body"] [ content]
+                    ]
+                ]
+            ]
+        , div [ class "modal-backdrop in" ] []
+        ]
