5896e0ac531676dc8f7b8b6e49eaff1e596efe8e
Luke Taylor <tekul.hs@gmail.com>
2018-11-22 22:07:21 +0000

    Add empty href attribute to drawer close button
    Allows us to ignore the "link clicked" event generated by Elm, which was causing
    the app to jump to the home page when the drawer was closed.
    
    Fixes #94.

--- frontend/app/src/Drawer.elm
+++ frontend/app/src/Drawer.elm
@@ -1,8 +1,8 @@
 module Drawer exposing (DrawerType(..), view)
 
-import Html exposing (Html, div, img, h1, p, text, ul, li, input)
-import Html.Attributes exposing (id, class, checked, src, type_, width)
-import Html.Events exposing (onClick, onCheck)
+import Html exposing (Html, div, h1, img, input, li, p, text, ul)
+import Html.Attributes exposing (checked, class, href, id, src, type_, width)
+import Html.Events exposing (onCheck, onClick)
 
 
 type DrawerType
@@ -11,13 +11,22 @@ type DrawerType
     | Summarise
     | Clarify
 
+
 drawerToString : DrawerType -> String
 drawerToString dt =
     case dt of
-        Connect -> "Connect"
-        Question -> "Question"
-        Summarise -> "Summarise"
-        Clarify -> " Clarify"
+        Connect ->
+            "Connect"
+
+        Question ->
+            "Question"
+
+        Summarise ->
+            "Summarise"
+
+        Clarify ->
+            " Clarify"
+
 
 view : Maybe DrawerType -> (DrawerType -> msg) -> Html msg
 view showDrawer toggleDrawer =
@@ -38,7 +47,7 @@ view showDrawer toggleDrawer =
                 , h1 []
                     [ text (drawerToString currentDrawer)
                     ]
-                , Html.a [ class "closebutton", onClick (toggleDrawer currentDrawer) ]
+                , Html.a [ href "#", class "closebutton", onClick (toggleDrawer currentDrawer) ]
                     [ img [ src "img/closeblack.png" ] [] ]
                 ]
 
@@ -46,11 +55,11 @@ view showDrawer toggleDrawer =
             case currentDrawer of
                 Connect ->
                     ( [ mkList
-                          [ "Do I know something about this already?"
-                          , "Has something like this ever happened to me?"
-                          , "Have I read about something like this?"
-                          , "What does this remind me of in the real world?"
-                          ]
+                            [ "Do I know something about this already?"
+                            , "Has something like this ever happened to me?"
+                            , "Have I read about something like this?"
+                            , "What does this remind me of in the real world?"
+                            ]
                       ]
                     , "connectblack.png"
                     , "connectpanel"
@@ -60,12 +69,12 @@ view showDrawer toggleDrawer =
                     ( [ p [] [ text "We want one sentence on what this story is all about." ]
                       , p [] [ text "It doesn't have to be your own words. If there's a sentence in the story that does the job, copy and paste it. Here's what to do if there isn't:" ]
                       , mkList
-                          [ "Skim the story fast, looking for good words or phrases."
-                          , "Write them down."
-                          , "Make a sentence by putting the words together with words of your own."
-                          , "Read your sentence aloud. If it doesn't make sense, change it to make it better."
-                          , "Take a last look at the story to see if you've missed any important point."
-                          ]
+                            [ "Skim the story fast, looking for good words or phrases."
+                            , "Write them down."
+                            , "Make a sentence by putting the words together with words of your own."
+                            , "Read your sentence aloud. If it doesn't make sense, change it to make it better."
+                            , "Take a last look at the story to see if you've missed any important point."
+                            ]
                       ]
                     , "summariseblack.png"
                     , "summarisepanel"
@@ -74,16 +83,16 @@ view showDrawer toggleDrawer =
                 Question ->
                     ( [ p [] [ text "Here are a few questions you could ask when you're reading. Feel free to think up more of your own." ]
                       , mkList
-                          [ "What does that sentence mean?"
-                          , "Does this part make sense to me?"
-                          , "How does the writer know that?"
-                          , "Is that fact or opinion?"
-                          , "How did they do that?"
-                          , "Why did they do that?"
-                          , "What if they had done it this way instead?"
-                          , "What question is this person trying to answer?"
-                          , "What happens next?"
-                          ]
+                            [ "What does that sentence mean?"
+                            , "Does this part make sense to me?"
+                            , "How does the writer know that?"
+                            , "Is that fact or opinion?"
+                            , "How did they do that?"
+                            , "Why did they do that?"
+                            , "What if they had done it this way instead?"
+                            , "What question is this person trying to answer?"
+                            , "What happens next?"
+                            ]
                       ]
                     , "questionblack.png"
                     , "questionpanel"
@@ -92,25 +101,25 @@ view showDrawer toggleDrawer =
                 Clarify ->
                     ( [ p [] [ text "Try to figure out what the word means using these methods:" ]
                       , mkList
-                          [ "Read a line or two around the word, looking for clues."
-                          , "Look for parts of words or whole words in the unknown word."
-                          , "Imagine the word isn't there and try another word or words in its place."
-                          ]
+                            [ "Read a line or two around the word, looking for clues."
+                            , "Look for parts of words or whole words in the unknown word."
+                            , "Imagine the word isn't there and try another word or words in its place."
+                            ]
                       ]
                     , "clarifyblack.png"
                     , "clarifypanel"
                     )
     in
-        div []
-            [ input
-                [ type_ "checkbox"
-                , id "toggle-drawer"
-                , onCheck (\_ -> toggleDrawer currentDrawer)
-                , checked (showDrawer /= Nothing)
-                ]
-                []
-            , div [ id "drawer", class panelStyle ]
-                [ drawerHeader
-                , div [ id "drawercontent" ] content
-                ]
+    div []
+        [ input
+            [ type_ "checkbox"
+            , id "toggle-drawer"
+            , onCheck (\_ -> toggleDrawer currentDrawer)
+            , checked (showDrawer /= Nothing)
             ]
+            []
+        , div [ id "drawer", class panelStyle ]
+            [ drawerHeader
+            , div [ id "drawercontent" ] content
+            ]
+        ]
