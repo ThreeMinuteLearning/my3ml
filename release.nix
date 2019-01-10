1e859ac26af70d23850f7e9ee55c83ab28ed4f41
Luke Taylor <tekul.hs@gmail.com>
2019-01-10 15:58:11 +0000

    Make nix build independent of compiler version
    Saves having to update the config every time the ghc version in
    the local dev environment changes.

--- release.nix
+++ release.nix
@@ -1,9 +1,13 @@
-{ compiler ? "ghc843" }:
+{ compiler ? "default" }:
 
 let
   pkgs = import <nixpkgs> {};
   dontCheck = pkgs.haskell.lib.dontCheck;
-  haskellPkgs = pkgs.haskell.packages."${compiler}".extend (self: super: {
+  hPkgs = if compiler == "default"
+              then pkgs.haskellPackages
+              else pkgs.haskell.packages.${compiler};
+
+  haskellPkgs = hPkgs.extend (self: super: {
     my3ml = self.callPackage ./my3ml.nix {};
     elm-export = self.callPackage ./elm-export.nix {};
     servant-elm = self.callPackage ./servant-elm.nix {};
